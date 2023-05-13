407f14c8ddb04ba1f2b0749955eb57ce3d35a556
iibrahim70 <aviknuhash70@gmail.com>
2023-05-13 07:10:56 +0600

    added navbar section

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/shared/Footer';
+import Navbar from '../pages/shared/Navbar';
 
 const Main = () => {
   return (
     <>
+      <Navbar/>
       <Outlet/>
       <Footer/>
     </>
